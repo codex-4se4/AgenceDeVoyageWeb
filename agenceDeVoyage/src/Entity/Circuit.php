a745bbfe555e0e6d7e98385e1f62c66dc4895550
Norchene Belhassen <nbelhassen@vermeg.com>
2021-11-17 14:16:52 +0100

    commit mohamed bourguiba

--- agenceDeVoyage/src/Entity/Circuit.php
+++ agenceDeVoyage/src/Entity/Circuit.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\CircuitRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CircuitRepository::class)
@@ -19,16 +20,32 @@ class Circuit
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank(message="Veuillez saisir une description.")
+     *@Assert\Length(
+     *      min = 20,
+     *      max = 255,
+     *      minMessage = "La description doit contenir au minimum {{ limit }} caractères.",
+     *      maxMessage = "La description doit contenir au maximu {{ limit }} caractères."
+     * )
      */
     private $description;
 
     /**
      * @ORM\Column(type="string", length=80)
+     * @Assert\NotBlank(message="Veuillez saisir un titre.")
+     * @Assert\Length(
+     *      min = 10,
+     *      max = 80,
+     *      minMessage = "Le titre doit contenir au minimum {{ limit }} caractères.",
+     *      maxMessage = "Le titre doit contenir au maximu {{ limit }} caractères."
+     * )
      */
     private $titre;
 
     /**
      * @ORM\Column(type="float")
+     * @Assert\NotBlank(message="Veuillez saisir un prix.")
+     * @Assert\Positive(message="Le prix doit être positif.")
      */
     private $prix;
 
