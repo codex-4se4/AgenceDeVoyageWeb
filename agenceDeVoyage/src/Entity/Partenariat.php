6a1e9bdf9ad6d64b32bdaa7b39a74996e76d4f4a
Wiemifaoui <73914179+wiemifaoui@users.noreply.github.com>
2021-11-24 20:24:04 +0100

    Update Partenariat.php

--- agenceDeVoyage/src/Entity/Partenariat.php
+++ agenceDeVoyage/src/Entity/Partenariat.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\PartenariatRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -36,6 +38,16 @@ class Partenariat
      * @ORM\Column(type="date")
      */
     private $DateFin;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Offre::class, mappedBy="Partenariat")
+     */
+    private $offres;
+
+    public function __construct()
+    {
+        $this->offres = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -89,4 +101,35 @@ class Partenariat
 
         return $this;
     }
+
+    /**
+     * @return Collection|Offre[]
+     */
+    public function getOffres(): Collection
+    {
+        return $this->offres;
+    }
+
+    public function addOffre(Offre $offre): self
+    {
+        if (!$this->offres->contains($offre)) {
+            $this->offres[] = $offre;
+            $offre->setPartenariat($this);
+        }
+
+        return $this;
+    }
+
+    public function removeOffre(Offre $offre): self
+    {
+        if ($this->offres->removeElement($offre)) {
+            // set the owning side to null (unless already changed)
+            if ($offre->getPartenariat() === $this) {
+                $offre->setPartenariat(null);
+            }
+        }
+
+        return $this;
+    }
 }
+
