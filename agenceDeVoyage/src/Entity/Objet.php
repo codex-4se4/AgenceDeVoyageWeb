8c3e6a968a8f14e676c0df8e9a5a02508ba7b053
fatmaBe <73782362+fatmaBe@users.noreply.github.com>
2021-11-26 05:44:27 +0100

    Update Objet.php

--- agenceDeVoyage/src/Entity/Objet.php
+++ agenceDeVoyage/src/Entity/Objet.php
@@ -2,36 +2,31 @@
 
 namespace App\Entity;
 
-use App\Repository\ObjetRepository;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
- * @ORM\Entity(repositoryClass=ObjetRepository::class)
+ * Objet
+ *
+ * @ORM\Table(name="objet")
+ * @ORM\Entity
  */
 class Objet
 {
     /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer", nullable=false)
      * @ORM\Id
-     * @ORM\GeneratedValue
-     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="IDENTITY")
      */
     private $id;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @var string
+     *
+     * @ORM\Column(name="nom", type="string", length=255, nullable=false)
      */
     private $nom;
-
-    /**
-     * @ORM\OneToOne(targetEntity=Reservation::class, mappedBy="objet", cascade={"persist", "remove"})
-     */
-    private $reservation;
-
-    /**
-     * @ORM\ManyToOne(targetEntity=Reservation::class, inversedBy="type")
-     * @ORM\JoinColumn(nullable=false)
-     */
-    private $objet;
 
     public function getId(): ?int
     {
@@ -50,37 +45,5 @@ class Objet
         return $this;
     }
 
-    public function getReservation(): ?Reservation
-    {
-        return $this->reservation;
-    }
 
-    public function setReservation(?Reservation $reservation): self
-    {
-        // unset the owning side of the relation if necessary
-        if ($reservation === null && $this->reservation !== null) {
-            $this->reservation->setObjet(null);
-        }
-
-        // set the owning side of the relation if necessary
-        if ($reservation !== null && $reservation->getObjet() !== $this) {
-            $reservation->setObjet($this);
-        }
-
-        $this->reservation = $reservation;
-
-        return $this;
-    }
-
-    public function getObjet(): ?Reservation
-    {
-        return $this->objet;
-    }
-
-    public function setObjet(?Reservation $objet): self
-    {
-        $this->objet = $objet;
-
-        return $this;
-    }
 }
