9953815b7f7ebf1fa0dca69c34a8809a16bd318e
fatmaBe <73782362+fatmaBe@users.noreply.github.com>
2021-11-26 11:37:54 +0100

    Update AppartementController.php

--- agenceDeVoyage/src/Controller/AppartementController.php
+++ agenceDeVoyage/src/Controller/AppartementController.php
@@ -3,11 +3,19 @@
 namespace App\Controller;
 
 use App\Entity\Appartement;
-use App\Form\AppartementType;
+use App\Entity\Role;
+use App\Entity\Utilisateur;
+use App\Form\AppartementFormType;
+use App\Form\RegistrationFormType;
+use App\Security\AppCustomAuthenticator;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Guard\GuardAuthenticatorHandler;
 
 class AppartementController extends AbstractController
 {
@@ -20,62 +28,92 @@ class AppartementController extends AbstractController
             'controller_name' => 'AppartementController',
         ]);
     }
-    /**
-     * @Route("/listAppartement",name="listAppartement")
-     */
-    public function listAppartement()
-    {
-        $appartements= $this->getDoctrine()->getRepository(Appartement::class)->findAll();
-        return $this->render("appartement/list.html.twig",array("tabAppartement"=>$appartements));
-    }
 
 
     /**
-     * @Route("/ajouterAppartement",name="ajouterAppartement")
+     * @Route("/ajouterAppartement", name="ajouter_appartement")
      */
-    public function ajouterAppartement(Request $request)
+    public function ajouterAppartement(Request $request): Response
     {
         $appartement = new Appartement();
-        $form= $this->createForm(AppartementType::class,$appartement);
+        $form = $this->createForm(AppartementFormType::class, $appartement);
         $form->handleRequest($request);
-        if($form->isSubmitted()){
-            $em= $this->getDoctrine()->getManager();
-            $em->persist($appartement);
-            $em->flush();
-            $this->getDoctrine()->getManager()->flush();
-            return $this->redirectToRoute("listAppartement");
+        $currentUser = $this->getDoctrine()->getRepository(Utilisateur::class)->findOneBy(array('login' => $this->getUser()->getUsername()));
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($appartement);
+            $entityManager->flush();
+            $appartements = $this->getDoctrine()->getRepository(Appartement::class)->findAll();
+
+
+            return $this->render('appartement/maisonsHote.html.twig', [
+                "appartements" => $appartements,
+                "current_user" => $currentUser
+            ]);
+
         }
-        return $this->render("appartement/ajouter.html.twig",array("formulaire"=>$form->createView()));
+
+
+
+        return $this->render('appartement/maisonHote-form.html.twig', [
+            'form_appartement' => $form->createView(),
+            "current_user" => $currentUser
+        ]);
+
+
     }
 
     /**
-     * @Route("/modifierAppartement/{id}",name="modifierAppartement")
+     * @Route("/modifier-appartement/{id}", name="modifier_appartement")
      */
-    public function modifierAppartement(Request $request,$id)
+    public function modifierAppartement(Request $request, int $id): Response
     {
-        $appartement = $this->getDoctrine()->getRepository(Appartement::class)->find($id);
-        $form= $this->createForm(appartementType::class,$appartement);
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $appartement = $entityManager->getRepository(Appartement::class)->find($id);
+        $form = $this->createForm(AppartementFormType::class, $appartement);
         $form->handleRequest($request);
-        if($form->isSubmitted()){
-            $em= $this->getDoctrine()->getManager();
-            $em->flush();
-            return $this->redirectToRoute("listAppartement");
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+            return $this->redirectToRoute("appartements");
         }
-        return $this->render("appartement/modifier.html.twig",array("formulaire"=>$form->createView()));
+        $currentUser = $this->getDoctrine()->getRepository(Utilisateur::class)->findOneBy(array('login' => $this->getUser()->getUsername()));
+        return $this->render("appartement/maisonHote-form.html.twig", [
+            "form_title" => "Modifier un appartement",
+            "form_appartement" => $form->createView(),
+            "current_user" => $currentUser
+        ]);
+    }
+
+
+    /**
+     * @Route("/supprimer-appartement/{id}", name="supprimer_appartement")
+     */
+    public function supprimerAppartement(int $id): Response
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $appartement = $entityManager->getRepository(Appartement::class)->find($id);
+        $entityManager->remove($appartement);
+        $entityManager->flush();
+
+        return $this->redirectToRoute("appartements");
     }
 
     /**
-     * @Route("/supprimerAppartement/{id}",name="supprimerAppartement")
+     * @Route("/appartements", name="appartements")
      */
-    public function supprimerAppartement($id)
+    public function appartements()
     {
-        $appartement= $this->getDoctrine()->getRepository(Appartement::class)
-            ->find($id);
-        $em= $this->getDoctrine()->getManager();
-        $em->remove($appartement);
-        $em->flush();
-        return $this->redirectToRoute("listAppartement");
+        $appartements = $this->getDoctrine()->getRepository(Appartement::class)->findAll();
+        $currentUser = $this->getDoctrine()->getRepository(Utilisateur::class)->findOneBy(array('login' => $this->getUser()->getUsername()));
+
+        return $this->render('appartement/appartements.html.twig', [
+            "appartements" => $appartements,
+            "current_user" => $currentUser
+        ]);
     }
 }
-
-
